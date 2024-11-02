092d8733f00fef13b671f8e3799cf40d156328a8
plic70 <jplichta70@gmail.com>
2024-11-02 13:06:03 -0500

    F: player movement and groups

--- main.py
+++ main.py
@@ -8,21 +8,32 @@ def main():
 	print(f"Screen height: {SCREEN_HEIGHT}")
 	screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 	clock = pygame.time.Clock()
+	updatable = pygame.sprite.Group()
+	drawable = pygame.sprite.Group()
+	Player.containers = (updatable, drawable)
 	player = Player(SCREEN_WIDTH/2, SCREEN_HEIGHT/2)
 	dt = 0
+	
+	
+
 	while True:
 		
 		for event in pygame.event.get():
 			if event.type == pygame.QUIT:
 				return
 		
-		
+		for thing in updatable:
+			thing.update(dt)
 
 		screen.fill("black")
-		player.draw(screen)
-		player.update(dt)
+		
+		for thing in drawable:
+			thing.draw(screen)
+		
 		pygame.display.flip()
 		dt = clock.tick(60) /1000
 
+		
+		
 if __name__=="__main__":
 	main()
