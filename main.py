dc3e375c22e54665f23c81476d305d66a3f3abc1
plic70 <jplichta70@gmail.com>
2024-11-02 21:11:20 -0500

    H: building shot

--- main.py
+++ main.py
@@ -1,18 +1,27 @@
+import sys
 import pygame
 from constants import *
 from player import Player
 from asteroids import Asteroid
 from asteroidfield import AsteroidField
+# ^---- all the imports
 
 def main():
 	print("Starting asteroids!")
 	print(f"Screen width: {SCREEN_WIDTH}")
 	print(f"Screen height: {SCREEN_HEIGHT}")
+	
+	#Create the game board
 	screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 	clock = pygame.time.Clock()
+
+	#Create the groups for things
 	updatable = pygame.sprite.Group()
 	drawable = pygame.sprite.Group()
 	asteroids = pygame.sprite.Group()
+	shots = pygame.sprite.Group()
+		
+	#Containers for things
 	Player.containers = (updatable, drawable)
 	Asteroid.containers = (asteroids, updatable, drawable)
 	AsteroidField.containers= (updatable)
@@ -31,6 +40,11 @@ def main():
 		
 		for thing in updatable:
 			thing.update(dt)
+			#thing.collision()
+		for asteroid in asteroids:
+			if asteroid.collision(player):
+				print("Game Over! You got hit")
+				sys.exit()
 
 		
 
